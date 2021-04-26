d5d6b1f4f91f05e03b41b1a071608818c07f954d
hitesh_khatri_mtx <Hitesh.khatri@mtxb2b.com>
2021-04-26 08:34:34 +0530

    pushing to git

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
-const port = process.env.PORT || 3000;
+const db = require('./queries');
+const port = process.env.PORT || 4000;
 var bodyParser = require('body-parser');
 
 var app = express();
 var server = http.createServer(app);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, './public')));
+app.post('/employee', db.createEmployee);
+app.post('/view', db.viewEmployee);
+app.post('/deleteEmployee', db.deleteEmployee);
+
 
 app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname, './public/form.html'));
 });
 
 server.listen(port, function() {
-	console.log('server is listening on port: 3000');
+	console.log('server is listening on port: 4000');
 });
---
+++ queries.js
@@ -0,0 +1,142 @@
+const { Client } = require('pg');
+const client = new Client({
+	connectionString: process.env.DATABASE_URL,
+	ssl: {
+		rejectUnauthorized: false
+	}
+});
+
+const createEmployee = (request, response) => {
+	client.connect();
+	const { name, employee_Id } = request.body;
+	client.query('INSERT INTO emp (name, employee_Id) VALUES ($1, $2)', [ name, employee_Id ], (error, result) => {
+		client.end();
+		if (error) {
+			console.log(JSON.stringify(error));
+			throw error;
+		}
+		response
+			.status(201)
+			.send(
+				'New employee has been added into the database with ID = ' +
+					request.body.employee_Id +
+					' and Name = ' +
+					request.body.name
+			);
+	});
+};
+
+const viewEmployee = (request, response) => {
+	client.connect();
+	const { employee_Id } = request.body;
+	const query = {
+		text: 'SELECT employee_id, name FROM emp WHERE employee_id = $1',
+		values: [ employee_Id ]
+	};
+
+	client.query(query, (error, result) => {
+		client.end();
+		if (error) {
+			console.log(JSON.stringify(error));
+			throw error;
+		}
+		if (result && result.rows[0]) {
+			response
+				.status(201)
+				.send(
+					'New employee has been added into the database with ID = ' +
+						result.rows[0].employee_id +
+						' and Name = ' +
+						result.rows[0].name
+				);
+		} else if (result) {
+			response.status(400).send('Record does not exist.');
+		}
+	});
+};
+
+const deleteEmployee = (request, response) => {
+	const { employee_Id } = request.body;
+	const query = {
+		text: 'DELETE FROM emp WHERE employee_Id = $1',
+		values: [ employee_Id ]
+	};
+	client.connect();
+	client.query(query, (error, result) => {
+		client.end();
+		if (error) {
+			console.log(JSON.stringify(error));
+			throw error;
+		}
+		console.log(JSON.stringify(result));
+		response.status(201).send('Record has been deleted.');
+	});
+};
+
+// const { Pool } = require('pg')
+// const pool = new Pool({
+//   user: 'hiteshkhatri',
+//   host: 'localhost',
+//   database: 'emp2',
+//   password: 'H8#9IjhKAB&#q1&*w1',
+//   port: 5432,
+//   idleTimeoutMillis: 2000,
+//   connectionTimeoutMillis: 2000,
+// });
+
+// const createEmployee = (request, response) => {
+//   console.log('I reched here.... ');
+//     const { name, employee_Id } = request.body
+//     pool.query('INSERT INTO emp (name, employee_Id) VALUES ($1, $2)', [name, employee_Id], (error, result) => {
+//       if (error) {
+//         console.log(JSON.stringify(error));
+//         throw error
+//       }
+//       response.status(201).send("New employee has been added into the database with ID = "+request.body.employee_Id+ " and Name = "+request.body.name);
+//     })
+//   }
+
+// const viewEmployee = (request, response) => {
+//   console.log('I reched here too ');
+//     const { employee_Id } = request.body;
+//     const query = {
+//       text: 'SELECT employee_id, name FROM emp WHERE employee_id = $1',
+//       values: [employee_Id],
+//     }
+
+//     pool.query(query, (error, result) => {
+//       if (error) {
+//         console.log(JSON.stringify(error));
+//         throw error
+//       }
+//       if(result && result.rows[0]){
+//         response.status(201).send("New employee has been added into the database with ID = "+result.rows[0].employee_id+ " and Name = "+result.rows[0].name);
+
+//       }else if(result){
+//         response.status(400).send('Record does not exist.');
+//       }
+//        })
+//   }
+
+//   const deleteEmployee = (request, response) => {
+//       const { employee_Id } = request.body;
+//       const query = {
+//         text: 'DELETE FROM emp WHERE employee_Id = $1',
+//         values: [employee_Id],
+//       }
+
+//       pool.query(query, (error, result) => {
+//         if (error) {
+//           console.log(JSON.stringify(error));
+//           throw error
+//         }
+//         console.log(JSON.stringify(result));
+//         response.status(201).send("Record has been deleted.");
+//       })
+//     }
+
+module.exports = {
+	createEmployee,
+	viewEmployee,
+	deleteEmployee
+};
