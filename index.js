4321ade53a469b30061c5668ef2570390eaca6d8
hitesh_khatri_mtx <Hitesh.khatri@mtxb2b.com>
2021-04-26 08:44:28 +0530

    push

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 var express = require('express');
-var http = require('http');
+var app = express();
 var path = require('path');
 const db = require('./queries');
 const port = process.env.PORT || 4000;
 var bodyParser = require('body-parser');
 
-var app = express();
-var server = http.createServer(app);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, './public')));
 app.post('/employee', db.createEmployee);
@@ -18,6 +16,6 @@ app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname, './public/form.html'));
 });
 
-server.listen(port, function() {
-	console.log('server is listening on port: 4000');
+const server = app.listen(process.env.PORT || 5000, () => {
+    console.log(`Express is working on port ${port}`);
 });
